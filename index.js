d9c6f6fff1b0cbb74f8dd3d8c6bf3d8cfee320d7
Jonathan Ray <jonathanray17@gmail.com>
2019-05-15 15:55:49 -0400

    testing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res)=> {
     });
 });
 app.post('/', (req, res)=> {
-    console.log(req);
+    console.log(req.body);
     res.setHeader("cows","moo");
     res.json({
         message : "Test successful",
