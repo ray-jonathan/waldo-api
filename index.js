5fc109529954298d80cac7c841243cb5325b0119
Jonathan Ray <jonathanray17@gmail.com>
2019-06-21 17:46:10 -0400

    adds websocket layer

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const {
     setBeacon,
     } = require('./controllers/locationdata');
 
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({ port: 8080 });
+wss.on('connection', ws => {
+    ws.on('message', message => {
+    console.log(`Received message => ${message}`);
+    });
+    ws.send('ho!');
+});
+
 app.use(express.json()); // Required for passing JSON to `req.body`
 app.use(express.urlencoded({extended: true}));
 
