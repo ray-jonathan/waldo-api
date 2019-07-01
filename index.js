9cf52f97c19540d3d38c952fcb90f8f00d41b7f6
Jonathan Ray <jonathanray17@gmail.com>
2019-07-01 17:10:12 -0400

    instantiates all returns

--- index.js
+++ index.js
@@ -141,6 +141,7 @@ app.post('/generate-user', async (req, res)=> {
     console.log(req.body);
     const {id, name, picture} = req.body;
     const user = await Phone.newUser(id, name, picture);
+    console.log("last step for user: ", user);
     res.json({
         user
     });
