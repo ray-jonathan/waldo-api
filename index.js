305b96d0000b7d692b7155f3592e5e14a5e61aaa
Jonathan Ray <jonathanray17@gmail.com>
2019-07-01 17:51:07 -0400

    moves socket conditions back to Switch cases

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const wss = new WebSocket.Server({
     clientTracking:true,
     server,
 });
+
+let decoyBool = false;
 wss.on('connection', (ws) => {
     console.log(" ");
     console.log("connected");
@@ -18,117 +20,162 @@ wss.on('connection', (ws) => {
         console.log("incoming socket say...");
         const messageJSON = JSON.parse(message);
         console.log(messageJSON);
-        if(messageJSON.type === "user"){
-            console.log("WS: USER CASE");
-            console.log("userFill: ");
-            const userFill = await Phone.setUserById(messageJSON.user.id, messageJSON.user.latitude, messageJSON.user.longitude);
-            // we'll want send the userFill object back so that the users have the name and picture of the player
-            // if (client !== ws && client.readyState === WebSocket.OPEN){
-            let numOfClients = 0;
-            wss.clients.forEach(async client => {
-                console.log("Client's ready state: ",client.readyState);
-                if (client !== ws && client.readyState === WebSocket.OPEN){
-                    numOfClients ++;
-                    client.send(JSON.stringify({
-                            type: "user",
-                            user: {
-                                [userFill.id] : {
-                                    name: userFill.name,
-                                    picture: userFill.picture,                
-                                    latitude: userFill.latitude,
-                                    longitude: userFill.longitude,
-                                }
-                            }
-                    }));
-                }
-            });
-            console.log(`Updated user information sent to ${numOfClients} phones.`);
-        }
-        else if (messageJSON.type === "flag"){
-            console.log("WS: Flag CASE");
-            if (messageJSON.decoy === false){
-                const phoneFill = await Beacon.setCoordinatesById(messageJSON.flag.id, messageJSON.flag.latitude, messageJSON.flag.longitude);
-                console.log("phoneFill: ");
-                console.log(phoneFill);
-                let numOfClients = 0;
-                wss.clients.forEach(async client => {
-                    if (client !== ws && client.readyState === WebSocket.OPEN){
-                        numOfClients ++;
-                        client.send(JSON.stringify({
-                            type: "flag",
-                            flag: {
-                                [messageJSON.flag.id] : {
-                                    latitude: messageJSON.flag.latitude,
-                                    longitude: messageJSON.flag.longitude,
-                                }
-                            },
-                        }));    
-                    }
-                });
-                console.log(`Updated flag information sent to ${numOfClients} phones.`);
-            }
-            else if ( messageJSON.decoy === true){
-                console.log("decoy enabled, sending dummy coordinates");
-                let numOfClients = 0;
-                wss.clients.forEach(async client => {
-                    if (client !== ws && client.readyState === WebSocket.OPEN){
-                        numOfClients ++;
-                        client.send(JSON.stringify({
-                            type: "flag",
-                            flag: {
-                                [messageJSON.flag.id] : {
-                                    latitude: messageJSON.flag.latitude,
-                                    longitude: messageJSON.flag.longitude,
-                                }
-                            },
-                            decoy: true,
-                        }));    
-                    }
-                });
-                console.log(`Updated flag information sent to ${numOfClients} phones.`);
-            }
-        }
-        else{
-            console.log(" ");
-            console.log("It wasn't a flag or a phone??");
-        }
-        // switch(messageJSON.type){
-        //     case("user"):
-        //         console.log("WS: USER CASE");
-        //         const userFill = await Phone.setUserById(messageJSON.user.id, messageJSON.user.latitude, messageJSON.user.longitude)
-        //         // we'll want send the userFill object back so that the users have the name and picture of the player
-        //         ws.send(JSON.stringify({
-        //             type: "user",
-        //             user: {
-        //                 [userFill.id] : {
-        //                     name: userFill.name,
-        //                     picture: userFill.picture,                
-        //                     latitude: userFill.latitude,
-        //                     longitude: userFill.longitude,
-        //                 }
-        //             }
-        //         }));
-        //         break;
-        //     case('flag'):
-        //         console.log("WS: Flag CASE");
-        //         const phoneFill = await Beacon.setCoordinatesById(messageJSON.flag.id, messageJSON.flag.latitude, messageJSON.flag.longitude)
+        // if(messageJSON.type === "user"){
+        //     console.log("WS: USER CASE");
+        //     console.log("userFill: ");
+        //     const userFill = await Phone.setUserById(messageJSON.user.id, messageJSON.user.latitude, messageJSON.user.longitude);
+        //     // we'll want send the userFill object back so that the users have the name and picture of the player
+        //     // if (client !== ws && client.readyState === WebSocket.OPEN){
+        //     let numOfClients = 0;
+        //     wss.clients.forEach(async client => {
+        //         console.log("Client's ready state: ",client.readyState);
+        //         if (client !== ws && client.readyState === WebSocket.OPEN){
+        //             numOfClients ++;
+        //             client.send(JSON.stringify({
+        //                     type: "user",
+        //                     user: {
+        //                         [userFill.id] : {
+        //                             name: userFill.name,
+        //                             picture: userFill.picture,                
+        //                             latitude: userFill.latitude,
+        //                             longitude: userFill.longitude,
+        //                         }
+        //                     }
+        //             }));
+        //         }
+        //     });
+        //     console.log(`Updated user information sent to ${numOfClients} phones.`);
+        // }
+        // else if (messageJSON.type === "flag"){
+        //     console.log("WS: Flag CASE");
+        //     if (messageJSON.decoy === false){
+        //         const phoneFill = await Beacon.setCoordinatesById(messageJSON.flag.id, messageJSON.flag.latitude, messageJSON.flag.longitude);
         //         console.log("phoneFill: ");
         //         console.log(phoneFill);
-        //         ws.send("************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************")
-        //         ws.send(JSON.stringify({
-        //             type: "flag",
-        //             flag: {
-        //                 [messageJSON.flag.id] : {
-        //                     latitude: messageJSON.flag.latitude,
-        //                     longitude: messageJSON.flag.longitude,
-        //                 }
+        //         let numOfClients = 0;
+        //         wss.clients.forEach(async client => {
+        //             if (client !== ws && client.readyState === WebSocket.OPEN){
+        //                 numOfClients ++;
+        //                 client.send(JSON.stringify({
+        //                     type: "flag",
+        //                     flag: {
+        //                         [messageJSON.flag.id] : {
+        //                             latitude: messageJSON.flag.latitude,
+        //                             longitude: messageJSON.flag.longitude,
+        //                         }
+        //                     },
+        //                 }));    
         //             }
-        //         }), {}, () => {console.log("flag info sent to phones");});
-        //         // console.log("flag info sent to phones");
-        //         break;
-        //     default: 
-        //         break;
-        // }        
+        //         });
+        //         console.log(`Updated flag information sent to ${numOfClients} phones.`);
+        //     }
+        //     else if ( messageJSON.decoy === true){
+        //         console.log("decoy enabled, sending dummy coordinates");
+        //         let numOfClients = 0;
+        //         wss.clients.forEach(async client => {
+        //             if (client !== ws && client.readyState === WebSocket.OPEN){
+        //                 numOfClients ++;
+        //                 client.send(JSON.stringify({
+        //                     type: "flag",
+        //                     flag: {
+        //                         [messageJSON.flag.id] : {
+        //                             latitude: messageJSON.flag.latitude,
+        //                             longitude: messageJSON.flag.longitude,
+        //                         }
+        //                     },
+        //                     decoy: true,
+        //                 }));    
+        //             }
+        //         });
+        //         console.log(`Updated flag information sent to ${numOfClients} phones.`);
+        //     }
+        // }
+        // else{
+        //     console.log(" ");
+        //     console.log("It wasn't a flag or a phone??");
+        // }
+        switch(messageJSON.type){
+            case("user"):
+                console.log("WS: USER CASE");
+                console.log("userFill: ");
+                const userFill = await Phone.setUserById(messageJSON.user.id, messageJSON.user.latitude, messageJSON.user.longitude);
+                // we'll want send the userFill object back so that the users have the name and picture of the player
+                // if (client !== ws && client.readyState === WebSocket.OPEN){
+                let numOfClients = 0;
+                wss.clients.forEach(async client => {
+                    console.log("Client's ready state: ",client.readyState);
+                    if (client !== ws && client.readyState === WebSocket.OPEN){
+                        numOfClients ++;
+                        client.send(JSON.stringify({
+                                type: "user",
+                                user: {
+                                    [userFill.id] : {
+                                        name: userFill.name,
+                                        picture: userFill.picture,                
+                                        latitude: userFill.latitude,
+                                        longitude: userFill.longitude,
+                                    }
+                                }
+                        }));
+                    }
+                });
+                console.log(`Updated user information sent to ${numOfClients} phones.`);
+                break;
+            case('flag'):
+                messageJSON.decoy ? console.log("WS: DECOY Flag CASE") : console.log("WS: REAL Flag CASE");
+                switch(messageJSON.decoy){
+                    case false: 
+                    // if (messageJSON.decoy === false){
+                        const phoneFill = await Beacon.setCoordinatesById(messageJSON.flag.id, messageJSON.flag.latitude, messageJSON.flag.longitude);
+                        console.log("phoneFill: ");
+                        console.log(phoneFill);
+                        let numOfClients = 0;
+                        wss.clients.forEach(async client => {
+                            if (client !== ws && client.readyState === WebSocket.OPEN){
+                                numOfClients ++;
+                                client.send(JSON.stringify({
+                                    type: "flag",
+                                    flag: {
+                                        [messageJSON.flag.id] : {
+                                            latitude: messageJSON.flag.latitude,
+                                            longitude: messageJSON.flag.longitude,
+                                        }
+                                    },
+                                }));    
+                            }
+                        });
+                        console.log(`Updated flag information sent to ${numOfClients} phones.`);
+                        break;
+                    case true:
+                        console.log("decoy enabled, sending dummy coordinates");
+                        let numOfClients = 0;
+                        wss.clients.forEach(async client => {
+                            if (client !== ws && client.readyState === WebSocket.OPEN){
+                                numOfClients ++;
+                                client.send(JSON.stringify({
+                                    type: "flag",
+                                    flag: {
+                                        [messageJSON.flag.id] : {
+                                            latitude: messageJSON.flag.latitude,
+                                            longitude: messageJSON.flag.longitude,
+                                        }
+                                    },
+                                    decoy: true,
+                                }));    
+                            }
+                        });
+                        console.log(`Updated flag information sent to ${numOfClients} phones.`);
+                        break;
+                }
+                break;
+            default: 
+                console.log(" ");
+                console.log(" ");
+                console.log("It wasn't a flag or a phone??");
+                console.log(" ");
+                console.log(" ");
+                break;
+        }        
     
     });
 });
